b22caf3482ee0fff509a4e24dffa3d7e62661a18
Deva0399 <dharshnikd@gmail.com>
2021-09-24 08:56:01 +0000

    java23

---
+++ src/demo/StringExample.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Fname="Deva";
+		String Lname="Dharshni";
+		String Fullname = Fname + " " + Lname;
+	    System.out.println("Full name is "+ Fullname);
+	    System.out.println("Full name is "+ Fullname.length());
+	    System.out.println("Name in Uppercase "+ Fullname.toUpperCase());
+	    
+	    String NewName = Fullname.replace(" ",",");
+	    System.out.println("New Name is "+ NewName);
+
+	}
+
+}
