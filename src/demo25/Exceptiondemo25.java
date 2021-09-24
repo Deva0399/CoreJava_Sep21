71208d9b6fb027cb8505d97ba0ab962fa247bd89
Deva0399 <dharshnikd@gmail.com>
2021-09-24 10:05:45 +0000

    newdemo25

---
+++ src/demo25/Exceptiondemo25.java
@@ -0,0 +1,31 @@
+package demo25;
+
+public class Exceptiondemo25 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		
+		int b[]= {12,1,3};
+		try {
+			//System.out.println(a/0);
+			System.out.println(b[1]);
+		}catch(NullPointerException e) {
+			e.printStackTrace();
+			System.out.println("Inside nullpoint exception");
+			
+		}catch(ArithmeticException e) {
+			e.printStackTrace();
+			System.out.println("Inside arithmetic exception");
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("Inside generic exception");
+		}finally {
+			System.out.println("Inside finally");
+		}
+		System.out.println("AfterException");
+
+	}
+
+}
